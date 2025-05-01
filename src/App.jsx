67258e7309032da14a56524914ae2d7847043965
João Henrique <joaohen.t8@gmail.com>
2025-04-30 23:18:23 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ export default function App() {
             <>
             <div className="footer">
                 2025 TechVison. <br />Inovando o futuro da tecnologia.Todos os direitos reservados
-                </div>
+            </div>
 
             </>
         </>
